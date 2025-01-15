79cb71320de4445fd9021d002e988156d40db34e
MuzzammilAfridi <118999159+MuzzammilAfridi@users.noreply.github.com>
2025-01-16 00:32:02 +0530

    try to deploy

--- api/index.js
+++ api/index.js
@@ -1,18 +1,18 @@
 
 const experss = require('express')
 const mongoose = require('mongoose')
-const userRoutes = require('./routes/userRoutes')
-const productRoutes = require('./routes/productRoutes')
+const userRoutes = require('../routes/userRoutes')
+const productRoutes = require('../routes/productRoutes')
 const cors = require('cors')
 const cookieParser = require('cookie-parser')
 const bodyParser = require('body-parser');
 const fileUpload = require('express-fileupload')
 const path = require('path')
 
+ 
 
 
-
-require('dotenv').config();
+require('dotenv').config(); 
 
 mongoose.connect(process.env.MONGO_URI)
 .then(()=>console.log("mongo db is connected Successfully")
@@ -45,9 +45,16 @@ app.use('/product', productRoutes)
 //     res.sendFile(path.resolve(_dirname, "Oasis", "dist", "index.html"))
 // })
 
+
+
 const PORT = process.env.PORT || 7070;
 const HOST = '0.0.0.0';
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhot:${PORT}`);
 });
+
+
+module.exports = (req, res) => {
+    app(req, res);
+  };
--- models/addToCart.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-const cartSchema = new mongoose.Schema({
-
-})
-
-const Cart = mongoose.model("Cart", cartSchema)
-
-module.exports = Cart
