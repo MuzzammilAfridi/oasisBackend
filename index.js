947a6c7878c3627f285a10fa01f9513f88adf4c0
MuzzammilAfridi <118999159+MuzzammilAfridi@users.noreply.github.com>
2025-01-17 19:01:57 +0530

    Adding url

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.use(express.json())
 app.use(bodyParser.json());
 app.use(cors({
     // origin : ['https://oasis-rho-pink.vercel.app'],
-    origin : ['http://localhost:5173'],
+    origin : ['https://oasis-rho-pink.vercel.app'],
     methods : ['GET', 'POST'],
     credentials : true
 }))
