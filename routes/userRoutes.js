d655c81321e5f5059883fa1b34cff8326d061d72
MuzzammilAfridi <118999159+MuzzammilAfridi@users.noreply.github.com>
2025-01-17 17:43:56 +0530

    Deploy againnnn

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcryptjs');
 const User = require('../models/user')
 const Product = require('../models/product')
 const jwt = require('jsonwebtoken')
